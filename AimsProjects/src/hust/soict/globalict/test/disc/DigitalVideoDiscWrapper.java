f63b8aeaa8dba84fce102f310fbf3a1e09b30c1c
ducdat18 <153488971+ducdat18@users.noreply.github.com>
2024-04-22 11:46:18 +0700

    AimsProjects
    Add new

---
+++ AimsProjects/src/hust/soict/globalict/test/disc/DigitalVideoDiscWrapper.java
@@ -0,0 +1,11 @@
+package hust.soict.globalict.test.disc;
+
+import hust.soict.globalict.aims.disc.DigitalVideoDisc;
+
+public class DigitalVideoDiscWrapper {
+    DigitalVideoDisc dvd;
+    DigitalVideoDiscWrapper(DigitalVideoDisc dvd) {
+        super();
+        this.dvd = dvd;
+    }
+}
